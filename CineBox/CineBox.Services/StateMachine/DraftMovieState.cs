d0d8613c6e8d4c90bee2fbbc07e595561c9a9feb
IPetrovic <ipetrokl@gmail.com>
2024-03-09 23:22:23 +0100

    RabbitMQ and EasyNetQ localhost

--- CineBox/CineBox.Services/StateMachine/DraftMovieState.cs
+++ CineBox/CineBox.Services/StateMachine/DraftMovieState.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Text;
 using AutoMapper;
 using Azure.Core;
 using CineBox.Model.Requests;
 using CineBox.Services.Database;
+using EasyNetQ;
 using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
 
 namespace CineBox.Services.StateMachine
 {
@@ -37,7 +40,33 @@ namespace CineBox.Services.StateMachine
             entity.StateMachine = "active";
 
             await _context.SaveChangesAsync();
-            return _mapper.Map<Model.ViewModels.Movie>(entity);
+
+            //RabbitMQ
+            //var factory = new ConnectionFactory { HostName = "localhost" };
+            //using var connection = factory.CreateConnection();
+            //using var channel = connection.CreateModel();
+
+            //channel.QueueDeclare(queue: "movie_added",
+            //                     durable: false,
+            //                     exclusive: false,
+            //                     autoDelete: false,
+            //                     arguments: null);
+
+            //const string message = "Hello World!";
+            //var body = Encoding.UTF8.GetBytes(message);
+
+            //channel.BasicPublish(exchange: string.Empty,
+            //                     routingKey: "movie_added",
+            //                     basicProperties: null,
+            //                     body: body);
+
+            var mappedEntity = _mapper.Map<Model.ViewModels.Movie>(entity);
+
+            //EasyNetQ
+            using var bus = RabbitHutch.CreateBus("host=localhost");
+            bus.PubSub.Publish(mappedEntity);
+            
+            return mappedEntity;
         }
 
         public override async Task<List<string>> AllowedActions()
---
+++ CineBox/Cinebox.Subscriber/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CineBox.Model.ViewModels;
+using EasyNetQ;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+
+//RabbitMQ
+//var factory = new ConnectionFactory { HostName = "localhost" };
+//using var connection = factory.CreateConnection();
+//using var channel = connection.CreateModel();
+
+//channel.QueueDeclare(queue: "movie_added",
+//                     durable: false,
+//                     exclusive: false,
+//                     autoDelete: false,
+//                     arguments: null);
+
+//Console.WriteLine(" [*] Waiting for messages.");
+
+//var consumer = new EventingBasicConsumer(channel);
+//consumer.Received += (model, ea) =>
+//{
+//    var body = ea.Body.ToArray();
+//    var message = Encoding.UTF8.GetString(body);
+//    Console.WriteLine($" [x] Received {message}");
+//};
+//channel.BasicConsume(queue: "movie_added",
+//                     autoAck: true,
+//                     consumer: consumer);
+
+//Console.WriteLine(" Press [enter] to exit.");
+//Console.ReadLine();
+
+//EasyNetQ
+using (var bus = RabbitHutch.CreateBus("host=localhost"))
+{
+    bus.PubSub.Subscribe<Movie>("test", HandleTextMessage);
+    Console.WriteLine("Listening for messages. Hit <return> to quit.");
+    Console.ReadLine();
+}
+
+void HandleTextMessage(Movie entity)
+{
+    Console.WriteLine($"Received: {entity?.Id}, {entity?.Title}");
+}
